e03780903aa7a66303c8543dcd6034721c1b4daa
Matt Olenick <matt.olenick@gmail.com>
2017-09-04 11:05:03 -0700

    Added terminal themes

--- xubuntu.sh
+++ xubuntu.sh
@@ -1,2 +1,13 @@
 # Disable apport
 sudo -k sed -i -r 's"enabled=1"enabled=0"' /etc/default/apport
+
+# Terminal themes
+mkdir ~/src
+cd ~/src
+git clone https://github.com/chriskempson/base16-xfce4-terminal
+cd ~/src/base16-xfce4-terminal
+./convert2themes
+sudo cp themes/*.theme /usr/share/xfce4/terminal/colorschemes/
+
+# Set up workspace
+mkdir ~/workspace
