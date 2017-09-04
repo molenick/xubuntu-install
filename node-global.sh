1894312c71705bb2d8d62127b83cc5a920ff98c7
Matt Olenick <matt.olenick@gmail.com>
2017-09-04 11:04:51 -0700

    Add unsafe warning comment to nvm install

--- node-global.sh
+++ node-global.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -O install_nvm.sh
 chmod u+x install_nvm.sh
+# Not exactly safe, run at your own risk
 ./install_nvm.sh
 source ~/.profile
 bash
