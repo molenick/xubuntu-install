50a34888d4043867f88cdd38f007d2f0509941d5
Matt <matt.olenick@gmail.com>
2017-09-12 11:47:57 -0700

    Added ephiphany-browser

--- apt-get-install.sh
+++ apt-get-install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # system
-sudo apt-get install aptitude
+sudo apt-get install aptitude keepass2
 
 # basic dev text
 sudo apt-get install sublime-text git
 
-# Chrome
-sudo apt-get install google-chrome-stable
+
+# Browser
+sudo apt-get install google-chrome-stable epiphany-browser
 
 # ruby dependencies
 sudo apt-get install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
